bf8ebe132553960c17d6fe025dd5b788a28c9450
Manuel Reich <errnesto@me.com>
2015-01-26 01:23:15 +0100

    add description

---
+++ app/js/jsx_components/graph/infos/description.jsx
@@ -0,0 +1,41 @@
+"use strict";
+
+var React = require('react');
+
+var Description = React.createClass({
+  listLinks: function (links) {
+    if (!links)
+      return false
+    
+    var linkList = links.map(function(link, i) {
+      return (
+        <li 
+          key       = {i}
+          className = "link-list-item">
+          <a href = {link}>
+            Quelle {i}
+          </a>
+        </li>
+      )
+    })
+    return (
+      <ul className = "link-list">
+        {linkList}
+      </ul>
+    )
+  },
+
+  render: function () {
+    return (
+      <div className = "description">
+        <p className = "text">
+          {this.props.description}
+        </p>
+       
+        {this.listLinks(this.props.links)}
+      </div>
+    );
+  }
+});
+
+module.exports = Description;
