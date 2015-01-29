f3f04e02710d20ec6c076680874a735ff5771e69
g-div <g-div@anche.no>
2015-01-29 04:42:43 +0100

    the map is now randomly pointing at certain countries, wow

--- app/js/jsx_components/graph/infos/map.jsx
+++ app/js/jsx_components/graph/infos/map.jsx
@@ -5,32 +5,39 @@ var d3 = require('d3');
 var topojson = require('topojson');
 
 var world = require('json!../../../api/world-110m.json');
+var names = require('dsv!../../../api/world-country-names.csv');
 
 var WorldMap = React.createClass({
-  getInitialState: function() {
-    return {country: 1};
-  },
   componentDidMount: function() {
       var canvas = this.refs.mapCanvas.getDOMNode();
       this.canvas = canvas.getContext("2d");
+
       this.renderMap();
   },
 
   drawMap: function() {
     this.canvas.clearRect(0, 0, this.props.width, this.props.height);
     this.canvas.fillStyle = "#bbb", this.canvas.beginPath(), this.path(this.land), this.canvas.fill();
-    this.canvas.fillStyle = "#ED1C24", this.canvas.beginPath(), this.path(this.countries[this.state.country]), this.canvas.fill();
+    this.canvas.fillStyle = "#ED1C24", this.canvas.beginPath(), this.path(this.countries[this.selectedCountry]), this.canvas.fill();
     this.canvas.strokeStyle = "#fff", this.canvas.lineWidth = .5, this.canvas.beginPath(), this.path(this.borders), this.canvas.stroke();
     this.canvas.strokeStyle = "#000", this.canvas.lineWidth = 2, this.canvas.beginPath(), this.path(this.globe), this.canvas.stroke();
   },
   rotate: function() {
+    console.log(this.props.country);
+
+    var country = names.filter(function(c) {
+      return this.props.country === c.name;
+    }.bind(this));
+
+    this.selectedCountry = country[0].id;
+
     d3.transition()
       .duration(1250)
       .each("start", function() {
         // this.title.text(countries[i = (i + 1) % n].name);
       })
       .tween("rotate", function() {
-        var p = d3.geo.centroid(this.countries[this.state.country]),
+        var p = d3.geo.centroid(this.countries[this.selectedCountry]),
             r = d3.interpolate(this.projection.rotate(), [-p[0], -p[1]]);
         return function(t) {
           this.projection.rotate(r(t));
@@ -56,6 +63,8 @@ var WorldMap = React.createClass({
     this.rotate();
   },
   render: function () {
+    this.rotate();
+
     return(
       <canvas className="globe" ref="mapCanvas" width={this.props.width} height={this.props.height}></canvas>
     );
