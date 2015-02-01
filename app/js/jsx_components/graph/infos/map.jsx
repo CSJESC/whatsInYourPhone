9d3b5f5fbf1e718c4895ac518954cf117727fd87
g-div <g-div@anche.no>
2015-02-01 02:09:09 +0100

    finishing the map component

--- app/js/jsx_components/graph/infos/map.jsx
+++ app/js/jsx_components/graph/infos/map.jsx
@@ -5,64 +5,85 @@ var d3 = require('d3');
 var topojson = require('topojson');
 
 var world = require('json!../../../api/world-110m.json');
-var names = require('dsv!../../../api/world-country-names.csv');
 
 var WorldMap = React.createClass({
+  getInitialState: function() {
+    return {
+      projection: {},
+      borders: {},
+      land: {},
+      countries: []
+    };
+  },
   componentDidMount: function() {
-      var canvas = this.refs.mapCanvas.getDOMNode();
-      this.canvas = canvas.getContext("2d");
+    var borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; });
+    var land = topojson.feature(world, world.objects.countries);
+    var countries = topojson.feature(world, world.objects.countries).features;
 
-      this.renderMap();
+    this.setState({borders: borders, land: land, countries: countries})
+    this.renderMap({canvas: this.getMap()});
   },
-
-  drawMap: function() {
-    this.canvas.clearRect(0, 0, this.props.width, this.props.height);
-    this.canvas.fillStyle = "#bbb", this.canvas.beginPath(), this.path(this.land), this.canvas.fill();
-    this.canvas.fillStyle = "#ED1C24", this.canvas.beginPath(), this.path(this.countries[this.selectedCountry]), this.canvas.fill();
-    this.canvas.strokeStyle = "#fff", this.canvas.lineWidth = .5, this.canvas.beginPath(), this.path(this.borders), this.canvas.stroke();
-    this.canvas.strokeStyle = "#000", this.canvas.lineWidth = 2, this.canvas.beginPath(), this.path(this.globe), this.canvas.stroke();
-  },
-  rotate: function() {
-
-    var country = names.filter(function(c) {
-      return this.props.country === c.name;
+  getCountry() {
+    var country = this.state.countries.filter(function(country) {
+      return country.properties.name === this.props.country;
     }.bind(this));
+    if (typeof country[0] !== 'undefined') var selectedCountry = country[0];
 
-    this.selectedCountry = country[0].id;
+    return selectedCountry;
+  },
+  getMap: function() {
+      var canvas = this.refs.mapCanvas.getDOMNode();
+      canvas = canvas.getContext("2d");
+
+      return canvas;
+  },
+  drawPolygon: function(params) {
+    params.canvas.fillStyle = params.fill;
+    params.canvas.beginPath();
+    params.path(params.area);
+    params.canvas.fill();
+
+    params.canvas.strokeStyle = params.strokeFill;
+    params.canvas.lineWidth = params.lineWidth;
+    params.canvas.beginPath();
+    params.path(params.borderPath);
+    params.canvas.stroke();
+  },
+  drawMap: function(params) {
+    params.canvas.clearRect(0, 0, this.props.width, this.props.height);
+
+    this.drawPolygon({ fill: '#bbb', area: this.state.land, strokeFill: '#fff', lineWidth : .5, borderPath : this.state.borders, canvas: params.canvas, path: params.path});
+    this.drawPolygon({ fill: '#f26522', area: params.selectedCountry, strokeFill: '#000', lineWidth: 2, borderPath: {type: "Sphere"}, canvas: params.canvas, path: params.path});
+  },
+  rotate: function(params) {
+    var selectedCountry = this.getCountry();
 
     d3.transition()
       .duration(1250)
-      .each("start", function() {
-        // this.title.text(countries[i = (i + 1) % n].name);
-      })
       .tween("rotate", function() {
-        var p = d3.geo.centroid(this.countries[this.selectedCountry]),
-            r = d3.interpolate(this.projection.rotate(), [-p[0], -p[1]]);
+        var p = d3.geo.centroid(selectedCountry),
+            r = d3.interpolate(this.state.projection.rotate(), [-p[0], -p[1]]);
         return function(t) {
-          this.projection.rotate(r(t));
-          this.drawMap();
+          this.state.projection.rotate(r(t));
+          if (!params.path) params.path = this.getPath({canvas: params.canvas});
+          this.drawMap({path: params.path, canvas: params.canvas, selectedCountry: selectedCountry});
         }.bind(this);
       }.bind(this));
   },
-  renderMap: function() {
-    this.projection = d3.geo.orthographic()
-      .scale(200)
-      .translate([this.props.width / 2, this.props.height / 2])
-      .clipAngle(90);
+  getPath(params) {
+    return d3.geo.path()
+      .projection(this.state.projection)
+      .context(params.canvas);
+  },
+  renderMap: function(params) {
+    this.setState({projection: d3.geo.orthographic().scale(200).translate([this.props.width / 2, this.props.height / 2]).clipAngle(90)});
 
-    this.path = d3.geo.path()
-      .projection(this.projection)
-      .context(this.canvas);
+    var path = this.getPath({canvas: params.canvas});
 
-    this.borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; });
-    this.countries = topojson.feature(world, world.objects.countries).features;
-    this.globe = {type: "Sphere"};
-    this.land = topojson.feature(world, world.objects.land);
-
-    this.rotate();
+    this.rotate({path: path, canvas: params.canvas});
   },
   render: function () {
-    this.rotate();
+    if (this.refs.mapCanvas) this.rotate({canvas: this.getMap()});
 
     return(
       <canvas className="globe" ref="mapCanvas" width={this.props.width} height={this.props.height}></canvas>
